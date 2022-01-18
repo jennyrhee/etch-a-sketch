63901089a3b58fa5fbc79774870df8935d66a139
jennyrhee <jennyirhee@gmail.com>
2022-01-18 14:07:38 -0600

    Add makeGrid and clearGrid functions

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+function makeGrid(size) {
+  const container = document.querySelector('.container');
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let i = 0; i < size; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      row.appendChild(square);  
+    }
+    container.appendChild(row);
+  }
+
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.addEventListener('mouseover', () => {
+  square.classList.add('square-black')
+}))
+}
+
 function clearGrid() {
   const squares = document.querySelectorAll('.square');
   squares.forEach(square => square.classList.remove('square-black'));
 }
 
-const container = document.querySelector('.container');
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  for (let i = 0; i < 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    row.appendChild(square);  
-  }
-  container.appendChild(row);
-}
-
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('mouseover', () => {
-  square.classList.add('square-black')
-}))
+makeGrid(16);
 
 const clearButton = document.getElementById('clear-btn');
 clearButton.addEventListener('click', clearGrid)

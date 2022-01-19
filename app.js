cc43724183a3c3a7236dad5c4604539e404d4ca6
jennyrhee <jennyirhee@gmail.com>
2022-01-19 14:39:53 -0600

    Change var to let

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
